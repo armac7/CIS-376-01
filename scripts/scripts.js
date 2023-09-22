1f45247f3661e3a3e02f2eb637f1ad14c2ababe0
Azyn7 <113379340+Azyn7@users.noreply.github.com>
2023-09-21 20:15:32 -0500

    Updated Index

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,11 @@
-$('document').ready(function(){
+$(document).ready(function(){
+    $('.nav-tabs #small-beginnings').tab('show');
+
     setTimeout(() => {
         AOS.init();
     }, 120);
 
-    
+    $(".nav-tabs a").click(function () {
+        $(this).tab('show');
+    });
 } );
