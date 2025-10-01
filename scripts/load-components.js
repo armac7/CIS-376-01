6417cecd040e1904ca5fa5d36409d66ed8154611
William Ryan McDonald <wmcdonald@una.edu>
2025-09-30 20:15:15 -0500

    Trying to fix loading for skills.

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,4 +1,7 @@
 $(function () {
     $('#navbar').load('/components/navbar.html');  
+    $('#skill-menu').load('components/skills.html', function() {
+        AOS.refresh(); // re-initialize animations after loading
+    });
     $('#footer').load('/components/footer.html');
 });
